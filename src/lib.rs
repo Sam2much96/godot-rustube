840ba6ff9042a53dd9e61d2a913f17ad0c3fab44
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2023-04-01 08:34:58 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,36 @@
 use rustube;
-use gdnative::tasks::Async;
-use gdnative::export::Varargs;
-use gdnative::object::TRef;
+//use gdnative::object::TRef;
 use gdnative::prelude::*;
-use gdnative::api::HTTPRequest;
+//use gdnative::api::HTTPRequest;
 
+
+#[allow(non_camel_case_types)]
 #[derive(NativeClass)]
-#[register_with(Self::register)]
-#[inherit(HTTPRequest)]
+#[inherit(Node)]
 pub struct RustubeNode;
 
+pub trait Method<RustubeNode>{}
 #[methods]
 impl RustubeNode {
-    fn new(_owner: TRef<HTTPRequest>) -> Self {
-        RustubeNode
+    fn new(_owner: &Node) -> Self {
+        godot_print!("Initializing Rustube");
+        RustubeNode{}
     }
 
-    #[export]
+    
     #[method]
-    fn _download_video(&self, #[base] owner: TRef<'_, HTTPRequest>, url: String) -> Async<String> {
-        async move {
-            rustube::download_best_quality(&url)
-                .await
-                .unwrap()
-                .to_str()
-                .expect("Path to VideoFIle")
-                .to_string()
-        }
+    async fn main() {
+    let url = "https://youtube.com/shorts/YCwou4oX12I?feature=share";
+    godot_print!("downloaded video to {:?}", rustube::download_best_quality(&url).await.unwrap());
     }
 
+    /*
     fn register(builder: &ClassBuilder<Self>) {
-        builder.method("_download_video", Self::_download_video_async).done();
+        /* Registers the Method*/
+        builder.method(stringify(Self::_download_video), Self::_download_video_async).done();
     }
 
+    
     async fn _download_video_async(
         &self,
         owner: TRef<'_, HTTPRequest>,
@@ -42,6 +40,9 @@ impl RustubeNode {
         let result = self._download_video(owner, url).await;
         Variant::from_str(&result)
     }
+
+    */
+    
 }
 
 fn init(handle: InitHandle) {
