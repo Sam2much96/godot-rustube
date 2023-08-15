12e71c6ff775eb77acf05f1d8782979606eff897
sam2much96 <sam2much96@gmail.com>
2023-08-15 19:14:33 +0100

    Successful Compile 3

--- src/godot-rustube/src/lib.rs
+++ src/godot-rustube/src/lib.rs
@@ -3,14 +3,14 @@ use rusty_ytdl::*;
 //use gdnative::tasks::Async;
 
 //simplifies async methods with custom macros
+use gdnative::tasks::Yield;
+use gdnative::tasks::{Async, AsyncMethod, Spawner};
 use macros::*;
 use std::rc::Rc;
-use gdnative::tasks::{Async, AsyncMethod, Spawner};
-use gdnative::tasks::Yield;
 
 // Futures Trait
-use futures::Future as future;
 use futures::task::Poll;
+use futures::Future as future;
 use std::pin::Pin;
 
 //GDNative Futures
@@ -18,18 +18,23 @@ use gdnative::tasks::Context as gdnativeContext;
 
 //GDNative VideoStream Class
 use gdnative::api::VideoStream;
+use gdnative::api::VideoStreamWebm;
+use gdnative::api::VideoStreamTheora;
+use gdnative::api::VideoPlayer;
+
 
 //Required Deps
 use futures::task::Context as futuresContext;
+use std::ops::Deref;
 
 #[allow(non_camel_case_types)]
 #[derive(NativeClass)]
 #[inherit(Node)]
 
 /*Define the Struct*/
-pub struct RustubeNode{
+pub struct RustubeNode {
     pub name: Rc<String>, //required for the custom async macro implementation
-    pub stream : Option<VideoStream>
+    pub stream: Option<VideoStreamTheora>,
 }
 
 //pub trait Method<RustubeNode> {}
@@ -38,22 +43,19 @@ pub struct RustubeNode{
 impl RustubeNode {
     fn new(_base: &Node) -> Self {
         godot_print!("Initializing Rustube");
-        RustubeNode{
+        RustubeNode {
             //placeholder accounts for algod node requirement in custom macro
-            name: Rc::new(String::new()), 
-            stream : None
+            name: Rc::new(String::new()),
+            stream: None,
         }
     }
 
     fn _ready(&self, _base: &Node) {
         godot_print!("Testing Native Plugin");
     }
-    
+
     /* Async Modve Original PR: https://github.com/godot-rust/gdnative/pull/709*/
-   
-
-    }
-
+}
 
 /*Defines Futures Trait for Download Method */
 #[allow(non_camel_case_types)]
@@ -61,21 +63,71 @@ impl RustubeNode {
 trait MyTrait {
     type VideoStream;
     type Future: future<Output = Self::VideoStream>; // Use associated type for Future
-    // Remove unnecessary associated types
+                                                     // Remove unnecessary associated types
 
-    fn download(&self) -> Self::Future; // Use associated type for Future
+    fn download(&self) -> Self::VideoStream; // Use associated type for Future
 }
+
+
+/* Implement trait bound `VideoStream: From<rusty_ytdl::Video> */
 
 /* Implements the trait for RustubeNode */
 impl MyTrait for RustubeNode {
-    type VideoStream = VideoStream; // Assuming VideoStream is your actual type
+    type VideoStream = String; // Assuming VideoStream is your actual type
     type Future = Pin<Box<dyn future<Output = Self::VideoStream>>>; // Implement Future type
 
-    fn download(&self) -> Self::Future {
-        Box::pin(async move {
+    fn download(&self) -> Self::VideoStream {
+        
+        let t = String::new();
+        let _ =Box::pin(async move {
             // Implement your async download logic here
-            todo!()
-        })
+            //todo!()
+                        /* Download Video Async Logic */
+            let video_url = "https://www.youtube.com/watch?v=FZ8BxMU3BYc"; // FZ8BxMU3BYc works too!
+            let video = Video::new(video_url).unwrap();
+
+            let stream = video.stream().await.unwrap();
+
+            while let Some(chunk) = stream.chunk().await.unwrap() {
+                // Do what you want with chunks
+                println!("{:#?}", chunk);
+            }
+
+            // Or direct download to path
+            let path = std::path::Path::new(r"test.mp3");
+
+            video.download(path).await.unwrap();
+
+            /*
+            //
+            // Or with options
+            //
+
+            let video_options = VideoOptions {
+                quality: VideoQuality::Lowest,
+                filter: VideoSearchOptions::Audio,
+                ..Default::default()
+            };
+
+            let video = Video::new_with_options(video_url, video_options).unwrap();
+
+            let stream = video.stream().await.unwrap();
+
+            while let Some(chunk) = stream.chunk().await.unwrap() {
+                // Do what you want with chunks
+                println!("{:#?}", chunk);
+            }
+
+            // Or direct download to path
+            let path = std::path::Path::new(r"test.mp3");
+
+            //video.download(path).await.unwrap();
+            video.download(path).await.unwrap();
+            */
+        });
+        t
+        //let y = t.deref();
+        //y
     }
 }
 
@@ -99,9 +151,9 @@ asyncmethods!(name, node, this,
           video.download(path).await.unwrap();
         //Ok(())
         };
-    
-    }    
-); 
+
+    }
+);
 */
 
 /*  Async Reciepe 1*/
@@ -130,7 +182,6 @@ impl futures::task::LocalSpawn for SharedLocalPool {
     }
 }
 
-
 use tokio::runtime::{Builder, Runtime};
 
 #[derive(NativeClass)]
@@ -143,8 +194,8 @@ impl AsyncExecutorDriver {
     fn new(_base: &Node) -> Self {
         AsyncExecutorDriver {
             runtime: Builder::new_current_thread()
-                .enable_io()    // optional, depending on your needs
-                .enable_time()  // optional, depending on your needs
+                .enable_io() // optional, depending on your needs
+                .enable_time() // optional, depending on your needs
                 .build()
                 .unwrap(),
         }
@@ -159,16 +210,13 @@ impl AsyncExecutorDriver {
             self.runtime
                 .block_on(async {
                     e.local_set
-                        .run_until(async {
-                            tokio::task::spawn_local(async {}).await
-                        })
+                        .run_until(async { tokio::task::spawn_local(async {}).await })
                         .await
                 })
                 .unwrap()
         })
     }
 }
-
 
 /*
 /* Async Recepie Original PR */
@@ -179,18 +227,14 @@ fn register_methods(builder: &ClassBuilder<RustubeNode>) {
 }
 */
 
-
 #[gdnative::init::callbacks]
 impl GDNativeCallbacks for RustubeNode {
-     fn nativescript_init(handle: InitHandle) {
-    
-        handle.add_class::<RustubeNode>();    
-        
+    fn nativescript_init(handle: InitHandle) {
+        handle.add_class::<RustubeNode>();
+
         gdnative::tasks::register_runtime(&handle);
         gdnative::tasks::set_executor(EXECUTOR.with(|e| *e));
-        
+
         handle.add_class::<AsyncExecutorDriver>();
     }
-
 }
-
