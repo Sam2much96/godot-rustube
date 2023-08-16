711cdcf76618919970b9f76bb44c854c14038803
sam2much96 <sam2much96@gmail.com>
2023-08-16 08:48:13 +0100

    Polishing Local Codebase Up

---
+++ Rustube.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+onready var yt_dl = $RustubeNode
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	yt_dl.test()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
